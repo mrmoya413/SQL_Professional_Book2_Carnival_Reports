8587336e4a624cd4b4ea0bc4d1bfad7314a7d057
mrmoya413 <mr.moya83@gmail.com>
2023-09-14 16:05:08 -0500

    In progress

---
+++ Chapter_4.sql
@@ -0,0 +1,9 @@
+/*Questions to Discuss in Class
+What data in Carnival is not normalized?
+After you identify non-normalized data, have suggestions for what new tables should be created to normalize the data.
+Practice: Student Exercises
+Given the following data, what would it look like in 1NF, 2NF and 3NF?
+
+Practice: Patients
+Normalize the following data so that it meets all requirements for 3NF.*/
+
